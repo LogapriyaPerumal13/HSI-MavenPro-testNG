ea5e593f24ace37d56d012de8ac9c6f9567f0b2a
LogapriyaPerumal13 <logapriya135@gmail.com>
2023-04-15 13:11:39 +0530

    LoginTest with valid, invalid input

--- selenium-maven-project/src/main/java/com/selenium/maven/LoginTest.java
+++ selenium-maven-project/src/main/java/com/selenium/maven/LoginTest.java
@@ -1,22 +1,25 @@
 package com.selenium.maven;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
 	WebDriver driver;
 
-	@BeforeTest
+	@BeforeMethod
 	public void launchBrowser()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Manoj\\Desktop\\HSI_Study\\Selenium Software\\New\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(40,TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.get("http://www.gmail.com");
 		
 	}
@@ -24,25 +27,32 @@ public class LoginTest {
 	@Test
 	public void logintest()
 	{
-		try {
-			driver.findElement(By.id("identifierId")).sendKeys("priyatestfortest@gmail.com");
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
-			Thread.sleep(3000);
-			driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("Priyatestfortest@HSI13");
-			driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
-				//span[@class='VfPpkd-vQzf8d']")).click();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}		
+			try {
+				driver.findElement(By.id("identifierId")).sendKeys("priyatestfortest@gmail.com");
+				driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+				Thread.sleep(3000);
+				driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("Priyatestfortest@HSI13");
+				driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 	}
 	
-	@AfterTest
-	public void closeBrowser() 
+	@Test
+	public void logintestinvalid()
 	{
-		driver.findElement(By.xpath("//img[@class='gb_h gbii']")).click();
-		driver.findElement(By.xpath("//div[contains(text(),'Sign out')]")).click();
-		driver.close();
+			try {
+				driver.findElement(By.id("identifierId")).sendKeys("priyatestfortest1@gmail.com");
+				driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+				Thread.sleep(3000);
+				/*driver.findElement(By.xpath("//input[@class='whsOnd zHQkBf']")).sendKeys("Priyatestfortest@HSI13");
+				driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
+				Thread.sleep(3000);*/
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
 	}
-		
-
 }
